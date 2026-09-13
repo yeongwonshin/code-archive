bda018f5300058be1bb95eba06ac7fc26b85e783
ywshin <shinforever12@gmail.com>
2026-09-14 00:35:15 +0900

    tweak

--- maximumSubarraySum.cpp
+++ maximumSubarraySum.cpp
@@ -25,3 +25,32 @@ int main() {
 
     return 0;
 }
+/* Kadane's Algorithm
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    vector<long long> arr(n);
+
+    // 입력 저장
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+
+    long long cur = 0;
+    long long ans = 0;
+
+    // Kadane's Algorithm
+    for (int i = 0; i < n; ++i) {
+        cur = max(0LL, cur + arr[i]);
+        ans = max(ans, cur);
+    }
+
+    cout << ans << '\n';
+
+    return 0;
+}*/
